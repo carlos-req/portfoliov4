d945a680a89c1dcedc3b8ad91231a4fa5ab6d459
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2025-05-20 22:43:10 -0400

    Add S3 storage bucket

--- payload.config.ts
+++ payload.config.ts
@@ -5,6 +5,7 @@ import { buildConfig } from "payload";
 import { Posts } from "./app/collections/Posts";
 import { Projects } from "./app/collections/Projects";
 import { Media } from "./app/collections/Media";
+import { s3Storage } from "@payloadcms/storage-s3";
 
 export default buildConfig({
   // If you'd like to use Rich Text, pass your editor here
@@ -22,6 +23,26 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URI || "",
     },
   }),
+  plugins: [
+    s3Storage({
+      collections: {
+        media: {
+          prefix: "media",
+        },
+      },
+      bucket: process.env.S3_BUCKET,
+      config: {
+        forcePathStyle: true,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+        region: process.env.S3_REGION,
+        endpoint: process.env.S3_ENDPOINT,
+      },
+    }),
+  ],
+
   // If you want to resize images, crop, set focal point, etc.
   // make sure to install it and pass it to the config.
   // This is optional - if you don't need to do these things,
